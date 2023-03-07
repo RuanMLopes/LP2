62ad4bd85af7805cc5b316b3473584fd47b0cea9
RuanMLopes <125613609+RuanMLopes@users.noreply.github.com>
2023-03-07 16:12:53 -0300

    Add files via upload

---
+++ Pvolume/Pvolume/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pvolume
+{
+    public partial class Form1 : Form
+    {
+        double altura, raio;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox3_Validated(object sender, EventArgs e)
+        {
+            if (!double.TryParse(textBox3.Text, out raio))
+            {
+                MessageBox.Show("Raio Inválido");
+                textBox3.Focus();
+            }
+            else if (raio <= 0)
+            {
+                MessageBox.Show("O raio deve ser maior que zero.");
+                textBox3.Focus();
+            }
+        }
+
+        private void TextBox2_Validated(object sender, EventArgs e)
+        {
+            if (!double.TryParse(textBox2.Text, out altura))
+            {
+                MessageBox.Show("Altura Inválida");
+                textBox2.Focus();
+            }
+            else if (altura <= 0)
+            {
+                MessageBox.Show("A altura deve ser maior que zero.");
+                textBox2.Focus();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void textBox3_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)(13))
+            {
+                SendKeys.Send("{TAB}");
+                e.Handled = true;
+            }
+        }
+
+        private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)(13))
+            {
+                SendKeys.Send("{TAB}");
+                e.Handled = true;
+            }
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            double volume;
+            volume = Math.PI * Math.Pow(raio, 2) * altura;
+            textBox1.Text = volume.ToString("N2");
+        }
+    }
+}
